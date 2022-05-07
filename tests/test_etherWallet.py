bfaa3f67ba273fd611020b60b60ec04513f14195
Abiyyu Yafi <yafiabiyyu@protonmail.com>
2022-05-07 17:52:32 +0700

    update test ether wallet

--- tests/test_etherWallet.py
+++ tests/test_etherWallet.py
@@ -1,46 +1,69 @@
 import brownie
-from brownie import web3
+from brownie import web3, accounts
 
 
 def test_owner_deposit_ether(contract, account):
-    deposit = web3.toWei(1, "ether")
-    contract.depositEther({"from": account[1], "value": deposit})
-    assert contract.getEtherBalance({"from": account[1]}) == deposit
+    value = web3.toWei("100", "ether")
+    contract.depositEther({"from": account[1], "value": value})
+    assert contract.balance() == value
 
 
 def test_owner_withdraw_ether(contract, account):
-    balance = web3.toWei("2", "ether")
-    contract.depositEther({"from": account[1], "value": balance})
+    deposit_value = web3.toWei("50", "ether")
+    contract.depositEther({"from": account[1], "value": deposit_value})
 
     contract.withdrawEther(
-        web3.toWei("2", "ether"), {"from": account[1]}
+        web3.toWei("5", "ether"), {"from": account[1]}
     )
+    assert contract.balance() == web3.toWei("45", "ether")
 
-    assert contract.getEtherBalance({"from": account[1]}) == 0
+
+def test_owner_transfer_ether(contract, account, nonOwner):
+    deposit_value = web3.toWei("50", "ether")
+    contract.depositEther({"from": account[1], "value": deposit_value})
+
+    contract.transferEther(
+        accounts[3], web3.toWei("5", "ether"), {"from": account[1]}
+    )
+    assert contract.balance() == web3.toWei("45", "ether")
+    assert accounts[3].balance() == web3.toWei("105", "ether")
 
 
-def test_owner_withdraw_ether_greter_balance(contract, account):
-    balance = web3.toWei("2", "ether")
-    contract.depositEther({"from": account[1], "value": balance})
+def test_revert_deposit_ether(contract, account, nonOwner):
     with brownie.reverts():
-        contract.withdrawEther(
-            web3.toWei("11", "ether"), {"from": account[1]}
+        contract.depositEther(
+            {"from": nonOwner, "value": web3.toWei("1", "ether")}
+        )
+        contract.depositEther(
+            {"from": account[1], "value": web3.toWei("0", "ether")}
         )
 
 
-def test_non_owner_update_claim_periode(contract, account):
+def test_revert_withdraw_ether(contract, account, nonOwner):
+    deposit_value = web3.toWei("50", "ether")
+    contract.depositEther({"from": account[1], "value": deposit_value})
+
     with brownie.reverts():
-        contract.updateClaimPeriod(2, {"from": account[2]})
-
-
-def test_non_owner_deposit_ether(contract, nonOwner):
-    balance = web3.toWei("2", "ether")
-    with brownie.reverts():
-        contract.depositEther({"from": nonOwner, "value": balance})
-
-
-def test_non_owner_withdraw_ether(contract, nonOwner):
-    with brownie.reverts():
+        contract.withdrawEther(deposit_value, {"from": nonOwner})
         contract.withdrawEther(
-            web3.toWei("2", "ether"), {"from": nonOwner}
+            web3.toWei("0", "ether"), {"from": account[1]}
+        )
+        contract.withdrawEther(
+            web3.toWei("70", "ether"), {"from": account[1]}
+        )
+
+
+def test_revert_transfer_ether(contract, account, nonOwner):
+    deposit_value = web3.toWei("50", "ether")
+    contract.depositEther({"from": account[1], "value": deposit_value})
+
+    with brownie.reverts():
+        contract.transferEther(
+            accounts[3], web3.toWei("5", "ether"), {"from": nonOwner}
+        )
+        contract.transferEther(
+            accounts[3], web3.toWei("0", "ether"), {"from": account[1]}
+        )
+        contract.transferEther(
+            accounts[3], web3.toWei("70", "ether"), {"from": account[1]}
         )
